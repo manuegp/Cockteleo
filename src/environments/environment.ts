0fe166618e1d706d3be679f0aa3cc7f0758754fd
Manuel Gonzalez <62593980+manuegp@users.noreply.github.com>
2024-08-18 00:49:48 +0200

    Update environment.ts critical

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,6 @@
 export const environment = {
     firebaseConfig : {
-        apiKey: 'AIzaSyCwtqOBKVCfI_pr5gQTcL8jB33l5cCb8pg',
-        authDomain: 'cockteleo-8724a.firebaseapp.com',
-        projectId: 'cockteleo-8724a',
-        storageBucket: 'cockteleo-8724a.appspot.com',
-        messagingSenderId: '282342032410',
-        appId: '1:282342032410:web:64e9a4a5a5a42c91dd3da3',
-        measurementId: 'G-DFP0GQ3WW4'
+        
       },
     production: true,
 };
